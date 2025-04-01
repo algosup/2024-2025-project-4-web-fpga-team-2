77d0a2fc9de6d82beaddf2b73f9ef3c602c761e9
LucasAub <lucas.aubard@algosup.com>
2025-04-01 16:16:43 +0200

    Update run_project.bat

--- run_project.bat
+++ run_project.bat
@@ -1,9 +1,9 @@
 @echo off
 REM Start the backend in a new command prompt window
-start cmd /k "cd src\Backend && if not exist node_modules npm install && npm run server"
+start cmd /k "cd src\Backend && if not exist node_modules (call npm install) && call npm run server"
 
 REM Start the frontend in a new command prompt window
-start cmd /k "cd src\Frontend && if not exist node_modules npm install && if not exist node_modules\d3 npm install d3 && npm run dev"
+start cmd /k "cd src\Frontend && if not exist node_modules (call npm install) && if not exist node_modules\d3 (call npm install d3) && call npm run dev"
 
 REM Open localhost in the default browser
 start "" "http://localhost:5173"
